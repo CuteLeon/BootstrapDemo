ce92e07275b490d5575a8a12412d04a91aaed5d5
Leon <Leon.Engineer@hotmail.com>
2020-08-14 00:10:10 +0800

    实现栅格容器和布局；

---
+++ BootstrapDemo/Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapDemo.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Grid()
+        {
+            return View();
+        }
+    }
+}
