38b9318643d4e81167d6e7bc5438735d641bb350
Leon <Leon.Engineer@hotmail.com>
2020-08-14 23:47:52 +0800

    完成警告窗

---
+++ BootstrapDemo/Controllers/ComponentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapDemo.Controllers
+{
+    public class ComponentController : Controller
+    {
+        public IActionResult Alert()
+        {
+            return View();
+        }
+    }
+}
