eec943e382e9a7e3571a130b664bf3f1f0708dfa
Leon <Leon.Engineer@hotmail.com>
2020-08-15 00:46:26 +0800

    完成按钮

--- BootstrapDemo/Controllers/ComponentController.cs
+++ BootstrapDemo/Controllers/ComponentController.cs
@@ -18,5 +18,10 @@ namespace BootstrapDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult Button()
+        {
+            return View();
+        }
     }
 }
