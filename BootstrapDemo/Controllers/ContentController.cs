13ea2fd331e7c22aff15fd7619fb9f8ccd4a5225
Leon <Leon.Engineer@hotmail.com>
2020-08-14 21:15:31 +0800

    实现内容排版；

---
+++ BootstrapDemo/Controllers/ContentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapDemo.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Typography()
+        {
+            return View();
+        }
+    }
+}
