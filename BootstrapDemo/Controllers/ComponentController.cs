e50b88f140508cfe3ddda9c78b34284a5450d537
Leon <Leon.Engineer@hotmail.com>
2020-08-14 23:59:19 +0800

    完成徽章

--- BootstrapDemo/Controllers/ComponentController.cs
+++ BootstrapDemo/Controllers/ComponentController.cs
@@ -8,5 +8,10 @@ namespace BootstrapDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult Badge()
+        {
+            return View();
+        }
     }
 }
