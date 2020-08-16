fe80b50eb3780c0ba378cbe79c5932d8a57b14a3
Leon <Leon.Engineer@hotmail.com>
2020-08-16 19:29:56 +0800

    实现表单

--- BootstrapDemo/Controllers/ComponentController.cs
+++ BootstrapDemo/Controllers/ComponentController.cs
@@ -48,5 +48,10 @@ namespace BootstrapDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
     }
 }
