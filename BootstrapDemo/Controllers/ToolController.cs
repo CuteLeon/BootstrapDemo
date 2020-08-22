9639e5566ca2d1aa0c50877f7c73ee6d14999281
Leon <Leon.Engineer@hotmail.com>
2020-08-22 16:01:36 +0800

    实现滚动监听；

---
+++ BootstrapDemo/Controllers/ToolController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapDemo.Controllers
+{
+    public class ToolController : Controller
+    {
+        public IActionResult ScrollSpy()
+        {
+            return View();
+        }
+    }
+}
