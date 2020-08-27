cd0ac20e57dee7df860d6d5eb81520b27171109a
Leon <Leon.Engineer@hotmail.com>
2020-08-27 22:47:24 +0800

    完成可见性

--- BootstrapDemo/Controllers/ToolController.cs
+++ BootstrapDemo/Controllers/ToolController.cs
@@ -73,5 +73,10 @@ namespace BootstrapDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult Visibility()
+        {
+            return View();
+        }
     }
 }
