de1468a49e785c24ce24f1ca3eaa4d5083f0cce6
Leon <Leon.Engineer@hotmail.com>
2020-09-04 22:13:13 +0800

    开发页面

---
+++ BootstrapDemo/Controllers/DevlopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapDemo.Controllers
+{
+    public class DevlopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
